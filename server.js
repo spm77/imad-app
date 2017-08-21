e42a4e0de39ad410ea6f99dd7b9032335f678199
spm77 <spm77@users.noreply.github.com>
2017-08-21 13:26:24 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article1', function (req, res) {
-  res.send('article1 has been requested');
+app.get('/article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article2', function (req, res) {
-  res.send('article2 has been requested');
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-
-app.get('/article3', function (req, res) {
-  res.send('article3 has been requested');
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {

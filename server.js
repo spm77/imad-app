cabea807c545201d0a96a108d6f3b92038258b7b
spm77 <spm77@users.noreply.github.com>
2017-08-25 15:34:56 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articles = {
-    articleOne: {
+    article-one: {
         title: 'Article One | Sam Mathews',
         heading: 'Article One',
         date: 'Aug 21 2017',
@@ -15,7 +15,7 @@ var articles = {
     `
     },
 
-    articleTwo: {
+    article-two: {
         title: 'Article Two | Sam Mathews',
         heading: 'Article Two',
         date: 'Aug 21 2017',
@@ -24,7 +24,7 @@ var articles = {
     `
     },
 
-    articleThree: {
+    article-three: {
         title: 'Article Three | Sam Mathews',
         heading: 'Article Three',
         date: 'Aug 22 2017',

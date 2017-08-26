dcbed1de13c25c0a5caaf1aa0dc4d96c370f518a
spm77 <spm77@users.noreply.github.com>
2017-08-26 16:17:25 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 // Counts number of times the button has been clicked.
-var button = document.getElementById("counter");
+var mybutton = document.getElementById("counter");
 var counter = 0;
 
-button.onClick = function() {
+mybutton.onClick = function() {
     // Make a request to the counter endpoint
     
     // Capture the response and store in a variable

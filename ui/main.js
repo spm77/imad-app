198f9fd5e338004ef5f78641921cf4b603439eda
spm77 <spm77@users.noreply.github.com>
2017-08-26 15:29:59 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,6 @@ myelement.innerHTML = 'New Value here...';
 
 var myimg = document.getElementById('madi');
 myimg.onclick = function() {
-    myimg.style.marginLeft = myimg.style.marginLeft + 15;
+    //myimg.style.marginLeft = myimg.style.marginLeft + 15;
+    alert('boo boo');
 }

dcd2b8370b22bbf38385056262e2ed1b22b14fec
spm77 <spm77@users.noreply.github.com>
2017-09-14 10:45:20 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,9 +122,22 @@ app.get('/submit-name', function(req, res) { //url/submit-name?name=param
     
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-  var articleName = req.params.articleName;
+  //var articleName = req.params.articleName;
+  pool.query("SELECT * FROM article WHERE article.title = " + req.params.articleName, function(err, result) {
+        if (err) {
+          res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0)
+            {
+                res.status(404).send('Article not found.');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
   res.send(createTemplate(articles[articleName]));
 });
 

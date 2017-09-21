36ef5282ab46ba854463601c47bb943a2d133a5d
spm77 <spm77@users.noreply.github.com>
2017-09-21 13:21:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'samandpriscilla',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: ' somevalue';
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }
+}));
 
 function createTemplate(data)
 {
@@ -111,13 +116,26 @@ app.post('/login', function(req, res){
                 var salt = dbstring.split('$')[2];
                 var hashedstring = hash(password, salt);
                 if (hashedstring === dbstring) {
+                    // Set the session
+                    req.session.auth.userId = {userid: result.rows[0].id;
+                    
                     res.send('credentials correct.');
+                    
+                    
                 } else {
                     res.send(403).send('username/password is invalid');
                 }
             }
         }
     });
+});
+   
+app.get('/check-login', function(req, res){
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        res.send('You are logged in: '+ req.session.auth.userId.toString());
+    } else {
+        res.send('You are not logged in.');
+    }
 });
     
 var pool = new Pool(config);

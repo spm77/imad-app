4223dc69915cf05e99645730aad1b72d21b192ac
spm77 <spm77@users.noreply.github.com>
2017-09-21 13:24:19 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
-    secret: ' somevalue';
+    secret: ' somevalue',
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }
 }));
 

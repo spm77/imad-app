be16933580c37884571299463877dea67d01b024
spm77 <spm77@users.noreply.github.com>
2017-09-21 11:19:43 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'samandpriscilla',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate(data)
 {
@@ -77,8 +79,10 @@ app.get('/hash/:input', function(req, res){
 });
 
 app.get('/create-user', function(req, res){
-   // accept username and password
-   var salt = crypto.getRandomBytes(128).toString('hex');
+   // accept username and password\
+   var username = req.body.username;
+   var password = req.body.password;
+   var salt = crypto.randomBytes(128).toString('hex');
    var dbstring = hash(password, salt);
    pool.query('INSERT into "user" (username, password) VALUES ($1, $2)',[username, dbstring], function(err, result){ 
         if (err) {
